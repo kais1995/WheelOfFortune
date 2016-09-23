5a5310944fd25929c1e20b2be2fc37dd80ac5593
kjk5419 <kjk5419@OZWDLND347-173.win.psu.edu>
2016-09-23 12:05:49 -0400

    added a function for number 8, and several fixes.

--- src/wheeloffortune/WheelOfFortune.java
+++ src/wheeloffortune/WheelOfFortune.java
@@ -10,11 +10,8 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class WheelOfFortune {
-      String play;
-      String letter;
-      Scanner scanner = new Scanner(System.in);
 
-  public static void main(String[] args) {
+public static void main(String[] args) {
       play();
   }
     public static void play(){
@@ -31,8 +28,9 @@ public class WheelOfFortune {
              + "2. Buy a vowel\n"
              + "3. Solve the puzzle\n"
              + "4. Quit\n"
+             + "8. Toggle puzzle reveal\n"
              + "9. Test letter input\n"
-             + "Enter choice:    ");
+             + "Enter choice: ");
      
      play = scanner.next();
  
@@ -41,9 +39,9 @@ public class WheelOfFortune {
          if ("1".equals(play)) { // if the user chooses 1 and spins the wheel it will call the method and spins the wheel and get a random value
          
          play = "You chose to spin the wheel.";
-         Spin();
+         Spin(); // the wheel would spin and would land on a number by calling/using the spin method and the puzzle mthod in the next line
          thePuzzle();
-
+            
          letter = scanner.next();
             if (letter.matches("[A-z]")){ // this checks and allowes only letters to be inputted
                 System.out.println("You chose the letter:" + letter); // for printing what the use chose
@@ -58,36 +56,46 @@ public class WheelOfFortune {
          if ("2".equals(play)){  // when the user chooses 2, he will buy a vowel
          play = "You chose to buy a vowel.";
 
-         System.out.println("What vowel will you buy?");
+         System.out.println("What vowel will you buy?"
+                 + "\nvowel 1"
+                 + "\nvowel 2");
+                 
+                
          letter = scanner.next();
          System.out.println("You chose to buy: " + letter);
      }
 
          if ("3".equals(play)){  // when the user chooses 3, they can try to solve the puzzle
          play = "Try solving the puzzle.";
+         
+     }
+          // if the user inputs 8, this will show the puzzle
+            String secretPhrase = "the quick brown fox jumps over the lazy dog";
+           if ("8".equals(play)) // when the user chooses 4, the game quits
+         {
+             System.out.println(secretPhrase);
+                         
+            int num; 
+        
+           Scanner in = new Scanner(System.in);
+        System.out.println("Press anything to go back to menu.");
+        num = in.nextInt();
+     
+             play();
+           
      }
        
-           if ("4".equals(play)) // when the user chooses 4, the game quits
-         {play = "You quit game.";
-
-         System.exit(0);
-     }
-                   if ("9".equals(play)) // when the user chooses 9, they can test if the game works properly
-         {play = "Yo are in QA Test Mode.";
-        
-     }
-        
          play();
      }
   
-  }
-
+}
+     
   
   
   
     public static void thePuzzle(){ // this method creates the puzzle.
                                     //  The below method creates the puzzle. And it waits for the users input. As the user enters a letter the puzzle is unvaild one by one.
-      String secretPhrase = "harambe was a good person"; // this is for the secret phrase
+      String secretPhrase = "the quick brown fox jumps over the lazy dog"; // this is for the secret phrase
       String guesses = " "; // for the user guesses
       Scanner guess = new Scanner (System.in);
       boolean notSolved = true;
@@ -108,8 +116,9 @@ public class WheelOfFortune {
       System.out.println("\nPick a Letter");
       String letter = guess.next();
       guesses += letter;
-      play();
+      
       }
+      
         System.out.println("\nCongratulations! You Win!"); // the message the user gets after he wins.
         System.exit(0); // to exit the program after the user has won
   }
@@ -124,9 +133,7 @@ public class WheelOfFortune {
               , " $800"," $900"," $900"," $5000"," BANKRUPT", " LOSE A TURN"};
       Random randomValue = new Random(); // this and the next line are for picking the value randomly
       int index = randomValue.nextInt(Values.length);
-      System.out.println("The wheel landed on:" + Values[index]); // this is for printing what the wheel landed on
+      System.out.println("The wheel landed on:" + Values[index]); // this is for printing what the wheel landed on - calls the index from the previous line
 
     }
-
-
   }
